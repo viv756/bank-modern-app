20ed09c3670375680d1f28fdc3068814f40ef499
*vivek <vrvivekofficial@gmail.com>
2024-05-20 09:34:40 +0530

    hero section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,32 @@
-import styles  from "./style";
+import styles from "./style";
 
-import {   Navbar,
-  Billing,
-  CardDeal,
-  Buisness,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-  Testimonials,
-  Hero } from './components'
-const App = () =>
+import { Navbar, Billing, CardDeal, Buisness, Clients, CTA, Stats, Footer, Testimonials, Hero } from "./components";
+const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-       <Navbar/>
+        <Navbar />
       </div>
-      
     </div>
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-       <Hero/>
+        <Hero />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
-      <Buisness/>
+        <Buisness />
         <Billing />
         <CardDeal />
         <Testimonials />
         <Clients />
         <CTA />
-        <Footer/>
+        <Footer />
       </div>
     </div>
-  </div>;
+  </div>
+);
 
 export default App;
